41a467e643c3aa8fd7b54dfc734645487e48dee8
Hüseyin Bartu kara <huseyinbartukara@gmail.com>
2023-07-28 15:57:51 +0300

    Initial Commit

---
+++ JokesAppSwiftUI/JokesAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppSwiftUIApp.swift
+//  JokesAppSwiftUI
+//
+//  Created by Bartu Kara on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
